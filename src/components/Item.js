b2944e6f0c47b4cf87925874cfd77649a168c25d
Brian <brianfsosa@gmail.com>
2023-07-23 19:04:29 -0300

    agregado de imagenes y diseño

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,16 +6,16 @@ function Item({codigo, nombre, precio, stock, descripcion,img}) {
     
         
 
-        <article className='w-64  lg:w-96 md:w-72 text-center lg:m-6'>
+        <article className='w-72  lg:w-96 md:w-72 text-center lg:m-6'>
           
           <div className="bg-gray-800 shadow-lg rounded-lg overflow-hidden">
       {/* Imagen del producto (puedes agregarla aquí si tienes una) */}
        <img src={img} alt={nombre} className="w-full h-64 object-cover" /> 
       
       <div className="p-6">
-        <h2 className="text-xl font-semibold text-white mb-4">{nombre}</h2>
+        <h2 className="text-4xl font-semibold text-white mb-4">{nombre}</h2>
         <p className="text-gray-300">{descripcion}</p>
-          <p className="text-green-500 font-semibold text-5xl text-center m-4">${precio}</p>
+        <p className='lg:m-10 md:m-10 m-6'><span className="text-green-500 font-semibold text-6xl mr-1">${precio}</span><i className="fa-solid fa-coins text-[#FFD700]"></i></p>
         <div>
             <Link to={`/item/${codigo}`} className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg">Ver detalle</Link>
         </div>
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import ItemCount from './ItemCount'
 
 function ItemDetail({nombre, precio, stock, descripcion, categoria, img}) {
@@ -10,9 +9,9 @@ function ItemDetail({nombre, precio, stock, descripcion, categoria, img}) {
        <img src={img} alt={nombre} className="lg:w-1/2 lg:h-1/4 md:w-1/2 md:h-1/4  object-cover" /> 
       
       <div className="p-6">
-        <h2 className="text-xl font-semibold text-white mb-4">{nombre}</h2>
-        <p className="text-gray-300 mb-4">{descripcion}</p>
-          <p className="text-green-500 font-semibold text-6xl lg:m-10 md:m-10 m-6">${precio}</p>
+        <h2 className="text-4xl font-semibold text-white mb-4">{nombre}</h2>
+            <p className="text-gray-300 mb-4">{descripcion}</p>
+            <p className='lg:m-10 md:m-10 m-6'><span className="text-green-500 font-semibold text-6xl mr-1">${precio}</span><i className="fa-solid fa-coins text-[#FFD700]"></i></p>
         <ItemCount stock={stock}/>
         <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg uppercase m-8 lg:m-10 md:m-10">Agregar al carrito</button>
       </div>
